afe2b60481777923e881dfe5dbd4d3fabdfeed9a
Jenipriya <cjenip@gmail.com>
2016-09-02 10:56:52 +0530

    Create inte.java

---
+++ inte.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class intcon
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+int n=Integer.parseInt(s1);
+System.out.print(n);
+}
+}
